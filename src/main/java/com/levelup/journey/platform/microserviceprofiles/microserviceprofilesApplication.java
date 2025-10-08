b02976eb8fae3d19d9ac8afec4df134a2b30ebe9
zGIKS <166082577+zGIKS@users.noreply.github.com>
2025-10-08 10:54:25 -0500

    feat: Add WebMvcConfigurer to redirect root to Swagger UI

--- src/main/java/com/levelup/journey/platform/microserviceprofiles/microserviceprofilesApplication.java
+++ src/main/java/com/levelup/journey/platform/microserviceprofiles/microserviceprofilesApplication.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -43,5 +46,15 @@ public class microserviceprofilesApplication implements CommandLineRunner {
         } else {
             System.out.println("Swagger UI is available at: " + swaggerUrl);
         }
+    }
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addRedirectViewController("/", "/swagger-ui/index.html");
+            }
+        };
     }
 }
